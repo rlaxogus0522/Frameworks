a8ca82b6766908a3b03404680178ef411da8f1ae
seanSummit <sean@summitcorp.co.kr>
2023-09-17 19:25:33 +0900

    gradle6.5 update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         classpath ("com.github.dcendents:android-maven-gradle-plugin:2.1")
-        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("com.android.tools.build:gradle:4.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.gms:google-services:4.3.5")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
